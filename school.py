3c238fbc6bca1c3e77e8b5e5f1c959a76e81fd2c
gonzalo lara <gonzaloflara@gmail.com>
2023-03-04 12:24:46 -0300

    ejercicio resuelto con cambio de nombre db a datab por problema de estilo

--- school.py
+++ school.py
@@ -1,15 +1,14 @@
 # pylint:disable=C0111,C0103
+import sqlite3
+conn = sqlite3.connect('data/school.sqlite')
+datab = conn.cursor()
 
 def students_from_city(db, city):
     """return a list of students from a specific city"""
-    pass  # YOUR CODE HERE
-
-
-# To test your code, you can **run it** before running `make`
-#   => Uncomment the following lines + run:
-#   $ python school.py
-#
-# import sqlite3
-# conn = sqlite3.connect('data/school.sqlite')
-# db = conn.cursor()
-# print(students_from_city(db, 'Paris'))
+    query ="""SELECT first_name
+    From students
+    WHERE  UPPER(birth_city) = upper(?)
+    """
+    db.execute(query,(city,))
+    results =  db.fetchall()
+    return results
